e87e3ab0eb297582e83713b463c7517b90de72a1
teambladerunner <teambladerunnder@gmail.com>
2014-10-24 17:27:19 +0530

    Server Call Changes
    Server Call Changes

--- webapps/miura/web/js/main.js
+++ webapps/miura/web/js/main.js
@@ -67,4 +67,17 @@ jQuery(function($) {'use strict',
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: false
 	});	
+
+
+	//goto top
+	$('#profileSettingsSubmit').click(function(event) {
+		//alert('Start');
+		//var data = JSON.stringify(jQuery('#registrationForm').serializeArray())
+		//var user = new Object();
+		//user.name = "Jesper";
+		//var data=JSON.stringify(user);
+		var data=JSON.stringify({name: "Mozilla"});
+		callServer("PUT","http://10.240.240.113:9000/user",data)
+		//alert('End');
+	});	
 });
--- webapps/miura/web/js/main/transport.js
+++ webapps/miura/web/js/main/transport.js
@@ -1,1 +1,28 @@
 //Ajax Calls to server or third party
+
+function callServer( type,  url,  data ){
+
+	alert('inside transaport');
+
+ jQuery.ajax({
+         type: type,
+         url: url,
+         data: data,
+         contentType: "application/json",
+         dataType: "json",
+         success: function (data, status, jqXHR) {
+              // do something
+              alert ('call to server succeeded')
+         },
+    
+         error: function (jqXHR, status) {           
+              // error handler
+               alert ('call to server did not succeed')
+
+         }
+
+     });
+
+
+
+}
