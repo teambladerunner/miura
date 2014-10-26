e43076fa63bfe4e842ac881622a9574d9da06d08
ashley <teambladerunnder@gmail.com>
2014-10-26 22:08:21 +0530

    CORS Enabled
    CORS Enabled

--- webapps/miura/web/js/main/prefs.js
+++ webapps/miura/web/js/main/prefs.js
@@ -1,5 +1,3 @@
-
-
 jQuery(function($) {'use strict',
 
 	$('#profileSettingsSubmit').click(function(event) {
@@ -9,7 +7,7 @@ jQuery(function($) {'use strict',
 		//user.name = "Jesper";
 		//var data=JSON.stringify(user);
 		var data=JSON.stringify({name: "Mozilla"});
-		callServer("PUT","http://10.240.240.113:9000/user",data)
+		callServer("PUT","http://10.240.240.113:9000/user",data,function () {populateForm1(data)})
 		//alert('End');
 	});	
 
@@ -20,6 +18,30 @@ jQuery(function($) {'use strict',
 		loadBundles(SESSION.locale);
 	});
 
+
+	function populateForm1(data) {
+		log("Populate Form");
+    
+    }
+
+	function populateForm (data) {
+  		$.each(data, function(name, val){
+   			 var $el = $('[name="'+name+'"]'),
+       		 type = $el.attr('type');
+
+		    switch(type){
+		        case 'checkbox':
+		            $el.attr('checked', 'checked');
+		            break;
+		        case 'radio':
+		            $el.filter('[value="'+val+'"]').attr('checked', 'checked');
+		            break;
+		        default:
+		            $el.val(val);
+    		}
+		});
+    
+    }
 
 
 });
--- webapps/miura/web/js/main/transport.js
+++ webapps/miura/web/js/main/transport.js
@@ -1,7 +1,8 @@
 //Ajax Calls to server or third party
 
-function callServer( type,  url,  data ){
+function callServer( type,  url,  data, functionSuccess ){
 
+jQuery.support.cors = true;
 
  jQuery.ajax({
          type: type,
@@ -9,17 +10,20 @@ function callServer( type,  url,  data ){
          data: data,
          contentType: "application/json",
          dataType: "json",
+         beforeSend : function (jqXHR, opts){
+              console.log("Before Sending");
+         },
          success: function (data, status, jqXHR) {
               // do something
               log('call to server succeeded');
+              functionSuccess(data);
          },
-    
-         error: function (jqXHR, status) {           
+         error: function (jqXHR, status, errorThrown) {           
               // error handler
-               log('call to server did not succeed, Status : '+status+ ' Error Message : '+jqXHR.responseText);
-
-
-         }
+              log('call to server did not succeed, Status : '+status+ ' Error Message : '+jqXHR.responseText+' Error Thrown : '+errorThrown);
+         },
+         timeout: 1000,
+         crossDomain: true 
 
      });
 
